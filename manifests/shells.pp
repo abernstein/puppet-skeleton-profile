b461e805a0f29be51c86b9b7fcf00ef96f0a8ff9
Aaron Bernstein <abernstein@godaddy.com>
2013-11-15 10:53:42 -0700

    Added a parameter to adjust shells

--- manifests/shells.pp
+++ manifests/shells.pp
@@ -1,3 +1,26 @@
-class profile::shells {
-  package { [ 'bash', 'zsh' ]: ensure => present, }
+# == Class: profile::shells
+# This class defines the concept of a profile to manage default shells.
+#
+# == Parameters:
+#
+# == Actions:
+#
+# == Requires:
+# Nothing.
+#
+# == Sample Usage:
+#  - invoke with the default attributes:
+#      class { 'profile::shells': }
+#
+#      class { 'profile::shells':
+#        shells = []
+#      }
+#
+#
+class profile::shells (
+  $shells = [ 'bash', 'zsh' ],
+) {
+  if (validate_array($shells)) {
+    package { $shells: ensure => present, }
+  }
 }
